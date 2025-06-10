72ad655f9887002f24e5551c0cb2484253bf9db9
vladislav-nii <v.mineev@refor.by>
2025-06-10 10:16:43 +0300

    context menu shadow and bottom right corner, fix keypress search, some style for panel zoom

--- debug.js
+++ debug.js
@@ -6,7 +6,7 @@
 
 const options = {};
 
-// options.ip = '127.0.0.1';
+options.ip = '192.168.1.111';
 // options.port = parseInt(process.argv[2]);
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
